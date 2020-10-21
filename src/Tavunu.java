a3694700206f2957e2c0b1a62c2c16afd5845589
Sohayab <72653595+Sohayab@users.noreply.github.com>
2020-10-21 04:30:48 -0500

    Update Tavunu.java

--- src/Tavunu.java
+++ src/Tavunu.java
@@ -7,8 +7,115 @@
  * pava -- items of status used for bargaining.
  *
  * @author Mithat Konar
- * @author Your Name
+ * @author Suhaib
  */
 public class Tavunu {
     /* See readme.md for what to do. */
+    private String name;
+    private int pava;
+    private int birthYear;
+    
+    public Tavunu()
+    {
+        name = "";
+        pava =0;
+        birthYear = Integer.MIN_VALUE;
+    }
+    
+    /**
+     * 
+     * @param name
+     * @param birthYear
+     * @param pava 
+     * constructors
+     */
+    public Tavunu(String name, int birthYear, int pava)
+    {
+        this.name= name;
+        this.pava= pava;
+        this.birthYear = birthYear;
+    }
+    /**
+     * 
+     * @return the name 
+     */
+    public String getName()
+    {
+        return name;
+    }
+    /**
+     * 
+     * @return the pava.
+     */
+    public int getPava()
+    {
+        return pava;
+    }
+    /**
+     * 
+     * @param int the pava to spend;
+     * spend pava if the amount is positive.
+     * @return 
+     */
+    public boolean spendPava(int pava)
+    {
+        if (pava >= 0 && pava <= this.pava)
+        {
+            this.pava -= pava;
+            return true;
+        }
+        return false;
+    }
+    /**
+     * 
+     * @param int the amount to add;receive pava if amount is positive.
+     * @return 
+     */
+    public boolean receivePava(int amount)
+    {
+        if (amount < 0)
+        {
+            return false;
+        }
+        pava += amount;
+        return true;
+    }
+    /**
+     * 
+     * @param name to set
+     * @return 
+     */
+    public boolean setName(String name)
+    {
+        if(name.startsWith("T") || name.startsWith("D"))
+        //if(name.charAt(0) == 'T' || name.charAt(0) == 'D')
+        {
+            this.name= name;
+            return true;
+        }
+        return false;
+    }
+    /**
+     * 
+     * @return the birthYear
+     */
+    public int getBirthYear()
+    {
+        return birthYear;
+    }
+    /**
+     * 
+     * @param birthYear to set 
+     */
+    public void setBirthYear(int birthYear)
+    {
+        this.birthYear = birthYear;
+    }
+    //toString to get a string representation of object.
+    public String toString()
+    {
+        String stringRepresentation = name + " born in "+ String.valueOf(birthYear)+
+                " has "+ String.valueOf(pava)+ " pava.";
+        return stringRepresentation;
+    }
 }
